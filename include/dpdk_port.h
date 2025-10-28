2b6c73e2ae72bd3102218e25d9aa271ea2f7bd0d
Ivan Sen <senivan1997@gmail.com>
2025-10-29 00:14:24 +0200

    header for port init

---
+++ include/dpdk_port.h
@@ -0,0 +1,21 @@
+#pragma once
+#ifndef DPDK_PORTS_H
+#define DPDK_PORTS_H
+#include <stdint.h>
+#include <rte_mempool.h>
+#include "app.h"
+
+struct dpdk_handle {
+    uint16_t lan_port;
+    uint16_t wan_port;
+    struct rte_mempool *mbuf_pool;
+};
+
+int eal_bootstrap(const char* progname);
+int vdev_create(const struct app_config *conf);
+int ports_configure(struct dpdk_handle *handle,
+                    uint16_t rx_desc, uint16_t tx_desc,
+                    uint32_t mbufs, uint32_t cache);
+void port_print_info(uint16_t port_id, const char* tag);
+
+#endif
